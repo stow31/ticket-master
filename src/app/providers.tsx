d0c25018f2f67f3a40682837074cd4be1079f297
Sophie Townsend <se.townsend31@gmail.com>
2025-01-06 19:37:30 +1100

    init

---
+++ src/app/providers.tsx
@@ -0,0 +1,9 @@
+'use client';
+import {Amplify} from 'aws-amplify';
+import { PropsWithChildren } from 'react';
+Amplify.configure({
+    
+})
+export default function Providers({children}: PropsWithChildren) {
+    return <>{children}</>
+}
